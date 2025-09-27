75bb28296b74f9de9c81b53075f94d5b6f099666
ambucha <ambuchatskiy@gmail.com>
2025-09-27 15:42:07 -0700

    commit two

--- lab-15.cpp
+++ lab-15.cpp
@@ -1,2 +1,30 @@
 // COMSC-210 | Lab 15 | Andrei Buchatskiy
 // IDE used : Visual Studio Code
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+// Create movie class with three private member variables: title, writer, year
+// Public member functions, getters and setters, and print
+class Movie {
+    private:
+        string title;
+        string writer;
+        int year;
+    public:
+        // Getters and setters
+        string getTitle() {return title;}
+        void setTitle(string t) {t = title;}
+        string getWriter() {return writer;}
+        void setWriter(string w) {w = writer;} 
+        int getYear() {return year;}
+        void setYear(string y) {y = year;}
+
+        // Print function
+        void print();
+};
+
+int main() {
+    
+}
