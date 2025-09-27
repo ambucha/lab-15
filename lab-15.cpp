fb960c35bc64007f1d31d25d0270bfc9aa903960
ambucha <ambuchatskiy@gmail.com>
2025-09-27 16:10:52 -0700

    commit four

--- lab-15.cpp
+++ lab-15.cpp
@@ -21,7 +21,7 @@ class Movie {
         string getWriter() {return writer;}
         void setWriter(string w) {w = writer;} 
         int getYear() {return year;}
-        void setYear(string y) {y = year;}
+        void setYear(int y) {y = year;}
 
         // Print function
         void print();
@@ -30,5 +30,32 @@ class Movie {
 int main() {
     // create the vector to hold the movies
     vector<Movie> movies;
-    // create variables to temporarily hold the class com
+
+    // create variables to temporarily hold the class components
+    string t;
+    string w;
+    int y;
+
+    // collect the data
+    ifstream fin ("data.txt");
+    if(fin.good()) {
+        // create a while loop that collects the data into temporary variables and then inputs it into the array at the end
+        while (fin >> t) {
+            fin >> y;
+            fin.ignore();
+            getline(fin, w);
+            Movie tmp;
+            tmp.setTitle(t);
+            tmp.setWriter(w);
+            tmp.setYear(y);
+            movies.push_back(tmp);
+        }
+        fin.close();
+    }
+    else {
+        cout << "Input file not there" << endl;
+    }
+
+    
+
 }
